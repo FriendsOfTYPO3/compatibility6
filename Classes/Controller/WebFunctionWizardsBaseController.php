f2e5e9224957cdc28ee321569db74130f208b7fd
Benjamin Mack <benni@typo3.org>
2014-12-08 22:03:19 +0100

    [!!!][TASK] Move EXT:func_wizard to legacy extension
    Web=>Functions=>Wizard is not needed
    anymore, as both wizard extensions hook
    into Web=>Functions directly since TYPO3 CMS 7.
    
    For compatibility reasons the
    WizardControllerModuleFunction is kept
    (e.g. for templavoila) but moved to EXT:compatibility6.
    
    The whole "func_wizard" Extension is not
    needed anymore and is removed.
    
    Resolves: #63687
    Releases: master
    Change-Id: I58e93652859bbdb816335322a9b964ec14cb84ee
    Reviewed-on: http://review.typo3.org/35171
    Reviewed-by: Cedric Ziel <cedric@cedric-ziel.com>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Susanne Moog <typo3@susannemoog.de>
    Tested-by: Susanne Moog <typo3@susannemoog.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

---
+++ Classes/Controller/WebFunctionWizardsBaseController.php
@@ -0,0 +1,89 @@
+<?php
+namespace TYPO3\CMS\Compatibility6\Controller;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * The Wizard function in the Web>Info module
+ * Creates a framework for adding wizard sub-sub-modules under the Wizard function in Web>Info
+ *
+ * @author Kasper Skårhøj <kasperYYYY@typo3.com>
+ */
+class WebFunctionWizardsBaseController extends \TYPO3\CMS\Backend\Module\AbstractFunctionModule {
+
+	/**
+	 * @var string
+	 */
+	public $function_key = 'wiz';
+
+	/**
+	 * Initialize.
+	 * Calls parent init function and then the handleExternalFunctionValue() function from the parent class
+	 *
+	 * @param object $pObj A reference to the parent (calling) object (which is probably an instance of an extension class to \TYPO3\CMS\Backend\Module\BaseScriptClass)
+	 * @param array $conf The configuration set for this module - from global array TBE_MODULES_EXT
+	 * @return void
+	 */
+	public function init(&$pObj, $conf) {
+		// OK, handles ordinary init. This includes setting up the menu array with ->modMenu
+		parent::init($pObj, $conf);
+		// Making sure that any further external classes are added to the include_once array.
+		// Notice that inclusion happens twice in the main script because of this!!!
+		$this->handleExternalFunctionValue();
+	}
+
+	/**
+	 * Modifies parent objects internal MOD_MENU array, adding items this module needs.
+	 *
+	 * @return array Items merged with the parent objects.
+	 */
+	public function modMenu() {
+		$GLOBALS['LANG']->includeLLFile('EXT:func_wizards/locallang.xlf');
+		$modMenuAdd = array(
+			$this->function_key => array()
+		);
+		$modMenuAdd[$this->function_key] = $this->pObj->mergeExternalItems($this->pObj->MCONF['name'], $this->function_key, $modMenuAdd[$this->function_key]);
+		$modMenuAdd[$this->function_key] = \TYPO3\CMS\Backend\Utility\BackendUtility::unsetMenuItems(
+			$this->pObj->modTSconfig['properties'],
+			$modMenuAdd[$this->function_key],
+			'menu.' . $this->function_key
+		);
+		return $modMenuAdd;
+	}
+
+	/**
+	 * Creation of the main content. Calling extObjContent() to trigger content generation from the sub-sub modules
+	 *
+	 * @return string The content
+	 */
+	public function main() {
+		$menu = \TYPO3\CMS\Backend\Utility\BackendUtility::getFuncMenu(
+			$this->pObj->id,
+			'SET[wiz]',
+			$this->pObj->MOD_SETTINGS['wiz'],
+			$this->pObj->MOD_MENU['wiz']
+		);
+
+		$content = '';
+		if (!empty($menu)) {
+			$menu = $GLOBALS['LANG']->getLL('wiz_lWizards', TRUE) . ': ' . $menu;
+			$content = $this->pObj->doc->section('', '<span class="nobr">' . $menu . '</span>');
+			$content .= $this->pObj->doc->spacer(20);
+		}
+
+		$content .= $this->extObjContent();
+		return $content;
+	}
+
+}
